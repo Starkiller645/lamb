0ddd8fc47f4ddd232a216a3e30e8333fbed7184b
Tallie Tye <tallie.tye@outlook.com>
2022-05-27 00:57:30 +0100

    Fixed issue where browser interpreted WS data as Blob, not String
    Signed-off-by: Tallie Tye <tallie.tye@outlook.com>

--- endpoints/bus.js
+++ endpoints/bus.js
@@ -1,61 +1,61 @@
-const https = require("https");
-const ws = require("ws");
-const fs = require("fs");
-const colors = require("colors");
+const https = require('https');
+const ws = require('ws');
+const fs = require('fs');
+const colors = require('colors');
 
 const lamb_bus = {
   init() {
     const bus_server = new ws.Server({ port: 30800 });
 
     // Extern server from the other websockets file
-    const err = require("./errors.js");
+    const err = require('./errors.js');
 
-    const config = JSON.parse(fs.readFileSync("./config.json"));
-    var http_server = undefined;
+    const config = JSON.parse(fs.readFileSync('./config.json'));
+    let http_server;
 
     if (config.keychain_file == undefined || config.privkey_file == undefined) {
       http_server = https.createServer({
-        cert: fs.readFileSync("./tls/cert.pem"),
-        key: fs.readFileSync("./tls/key.pem"),
+        cert: fs.readFileSync('./tls/cert.pem'),
+        key: fs.readFileSync('./tls/key.pem'),
       });
       err.warn(
         "Using insecure websocket communication because keychain_file and/or privkey_file weren't specified in config.json",
-        "websockets"
+        'websockets',
       );
     } else {
       http_server = https.createServer({
-        cert: fs.readFileSync(config.keychain_file, "utf8"),
-        key: fs.readFileSync(config.privkey_file, "utf8"),
+        cert: fs.readFileSync(config.keychain_file, 'utf8'),
+        key: fs.readFileSync(config.privkey_file, 'utf8'),
       });
     }
 
     const broadcast_server = new ws.WebSocketServer({ server: http_server });
 
-    broadcast_server.on("connection", (sock) => {
-      sock.send(
-        JSON.stringify({
-          ident: "websockets",
-          data: {
-            state: "CONNECTED",
-          },
-        })
-      );
+    broadcast_server.on('connection', (sock) => {
+      console.log('Received new WSS:// connection');
+      sock.send(String(JSON.stringify({
+        ident: 'websockets',
+        data: {
+          state: 'CONNECTED',
+          phase: 'waiting',
+        },
+      })), { binary: false });
     });
 
-    bus_server.on("connection", (sock) => {
-      sock.on("message", (data) => {
-        for (var client of broadcast_server.clients) {
+    bus_server.on('connection', (sock) => {
+      sock.on('message', (data) => {
+        for (const client of broadcast_server.clients) {
           if (client.readyState === ws.OPEN && client !== sock) {
-            client.send(data);
+            client.send(data, { binary: false });
           }
         }
       });
     });
 
-    console.log("[/websockets]".bold.brightMagenta, "Initialised");
+    console.log('[/websockets]'.bold.brightMagenta, 'Initialised');
     console.log(
-      "[/bus]".bold.brightBlue,
-      "Initialised LAMB internal broadcast bus"
+      '[/bus]'.bold.brightBlue,
+      'Initialised LAMB internal broadcast bus',
     );
 
     http_server.listen(3081);
